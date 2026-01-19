483daaf422944c7428b741ced4e0d771e9db3229
Mathieu Harison <hei.mathieu.95@gmail.com>
2026-01-19 10:10:14 +0300

    DishIngredient

---
+++ src/main/java/DishIngredient.java
@@ -0,0 +1,60 @@
+public class DishIngredient {
+    private Integer id;
+    private Dish dish;
+    private Ingredient ingredient;
+    private Double quantityRequired;
+    private UnitEnum unit; // Enum PCS, KG, L
+
+    public DishIngredient(Integer id, Dish dish, Ingredient ingredient,
+                          Double quantityRequired, UnitEnum unit) {
+        this.id = id;
+        this.dish = dish;
+        this.ingredient = ingredient;
+        this.quantityRequired = quantityRequired;
+        this.unit = unit;
+    }
+
+    public Double getCost() {
+        return ingredient.getPrice() * quantityRequired;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Double getQuantityRequired() {
+        return quantityRequired;
+    }
+
+    public void setQuantityRequired(Double quantityRequired) {
+        this.quantityRequired = quantityRequired;
+    }
+
+    public UnitEnum getUnit() {
+        return unit;
+    }
+
+    public void setUnit(UnitEnum unit) {
+        this.unit = unit;
+    }
+}
